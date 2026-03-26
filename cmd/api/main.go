9380a965f7efbcb74b4f7fccbda676cca37dc576
l122 <72576310+l122@users.noreply.github.com>
2026-03-26 21:41:26 +0500

    Dev (#35)
    * remove all but main
    
    * rename module
    
    * display hello
    
    * display version

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"os/signal"
-	"syscall"
-	"time"
-
-	"expense-tracker/internal/server"
 )
 
 var (
@@ -18,29 +12,11 @@ var (
 	BuildDate  = "n/a"
 )
 
-func gracefulShutdown(apiServer *http.Server, done chan bool) {
-	// Create context that listens for the interrupt signal from the OS.
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	// Listen for the interrupt signal.
-	<-ctx.Done()
-
-	log.Println("shutting down gracefully, press Ctrl+C again to force")
-	stop() // Allow Ctrl+C to force shutdown
-
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := apiServer.Shutdown(ctx); err != nil {
-		log.Printf("Server forced to shutdown with error: %v", err)
-	}
-
-	log.Println("Server exiting")
-
-	// Notify the main goroutine that the shutdown is complete
-	done <- true
+func version(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+	fmt.Fprintf(w, "AppVersion: %v\n", AppVersion)
+	fmt.Fprintf(w, "CommitSHA: %v\n", CommitSHA)
+	fmt.Fprintf(w, "BuildDate: %v\n", BuildDate)
 }
 
 func main() {
@@ -51,20 +27,8 @@ func main() {
 	log.Printf("BuildDate: %v", BuildDate)
 
 	log.Println("Creating server")
-	server := server.NewServer()
 
-	// Create a done channel to signal when the shutdown is complete
-	done := make(chan bool, 1)
+	http.HandleFunc("/", version)
 
-	// Run graceful shutdown in a separate goroutine
-	go gracefulShutdown(server, done)
-
-	err := server.ListenAndServe()
-	if err != nil && err != http.ErrServerClosed {
-		panic(fmt.Sprintf("http server error: %s", err))
-	}
-
-	// Wait for the graceful shutdown to complete
-	<-done
-	log.Println("Graceful shutdown complete.")
+	http.ListenAndServe(":8080", nil)
 }
