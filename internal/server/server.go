cbfa9934edacb59db040d98bb40715fd7c9c19a1
mino <minolyndo@gmail.com>
2026-04-25 18:02:10 +0300

    get port from env

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,7 +2,12 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 type Server struct {
@@ -10,8 +15,20 @@ type Server struct {
 }
 
 func NewServer() *http.Server {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		portStr = "8080"
+	}
+
+	port, _ := strconv.Atoi(portStr)
+
 	NewServer := &Server{
-		port: 8080,
+		port: port,
 	}
 
 	server := &http.Server{
