a52f89c9e109e57607325b5148f035939d7e674b
l122 <72576310+l122@users.noreply.github.com>
2026-05-04 20:58:43 +0300

    Extract components UI (#48) #minor

--- cmd/web/efs.go
+++ cmd/web/efs.go
@@ -3,15 +3,33 @@ package web
 import (
 	"embed"
 	"html/template"
+	"io/fs"
 	"log"
+	"strings"
 )
 
-//go:embed features/*/*.gohtml
+//go:embed styles.css features components
 var Files embed.FS
 
 func ParseTemplates() *template.Template {
-	// Add patterns for the root directory and feature subdirectories
-	templates, err := template.ParseFS(Files, "features/*/*.gohtml")
+	var filenames []string
+
+	// Recursively walk the embedded filesystem to find all .gohtml files
+	err := fs.WalkDir(Files, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() && strings.HasSuffix(path, ".gohtml") {
+			filenames = append(filenames, path)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Parse all discovered template files
+	templates, err := template.ParseFS(Files, filenames...)
 	if err != nil {
 		log.Fatal(err)
 	}
