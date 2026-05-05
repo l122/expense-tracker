8f80286e535bb52a553cae7532ec66adfe47e0f7
mino <minolyndo@gmail.com>
2026-05-05 20:43:51 +0300

    change .gohtml to .html

--- cmd/web/efs.go
+++ cmd/web/efs.go
@@ -14,12 +14,12 @@ var Files embed.FS
 func ParseTemplates() *template.Template {
 	var filenames []string
 
-	// Recursively walk the embedded filesystem to find all .gohtml files
+	// Recursively walk the embedded filesystem to find all .html files
 	err := fs.WalkDir(Files, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
-		if !d.IsDir() && strings.HasSuffix(path, ".gohtml") {
+		if !d.IsDir() && strings.HasSuffix(path, ".html") {
 			filenames = append(filenames, path)
 		}
 		return nil
