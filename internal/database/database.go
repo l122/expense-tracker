bcc2e0ca32bbc82092418e80ccb269d855ed0c4a
minolyndo <minolyndo@gmail.com>
2026-06-16 18:25:04 +0500

    extract getusers.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -126,30 +126,3 @@ func (s *service) Close() error {
 	log.Printf("Disconnected from database: %s", dburl)
 	return s.db.Close()
 }
-
-func (s *service) GetUsers() ([]domain.User, error) {
-	// TODO:get []User from db
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-
-	rows, err := s.db.QueryContext(ctx, "SELECT id, name, email, username, role FROM Users")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var result []domain.User
-	for rows.Next() {
-		user := domain.User{}
-		if err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Name, &user.Role); err != nil {
-			return nil, err
-		}
-
-		result = append(result, user)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
